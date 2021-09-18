88ed90bb255b376e63086de6e8257f7d7f7d07cb
G-Peña <89948235+g-pena@users.noreply.github.com>
2021-09-18 18:40:49 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+    //variables
+    double sales;
+    string name;
+
+    //Salesperson's name
+   
+    Console.WriteLine ("Enter salesperson's name:"); 
+    name = Console.ReadLine();
+
+    //Sales amount
+    Console.WriteLine("Enter sales amount:");
+    sales = Convert.ToDouble(Console.ReadLine());
+
+    //Calculate commission 200 + (0.09 * SALESAMOUNT)
+    //Display preformance status
+    
+    
+
+
+
+
+
+
+
+
+  }
+}
