f73f74f520c1a24e08ee7699b76ff3e21fb35bb7
G-Peña <89948235+g-pena@users.noreply.github.com>
2021-09-20 00:53:51 +0000

    added comment for question

--- main.cs
+++ main.cs
@@ -5,6 +5,7 @@ class Program {
     //variables
     double sales;
     string name;
+    double commission;
 
     //Salesperson's name
    
@@ -16,9 +17,39 @@ class Program {
     sales = Convert.ToDouble(Console.ReadLine());
 
     //Calculate commission 200 + (0.09 * SALESAMOUNT)
+
+    commission = 200+(0.09*sales);
+    Console.WriteLine("Sales commission for "+ name);//this is where I am having an issue putting in .Write instead of .WriteLine.
+    Console.WriteLine("is $" + commission);
+
     //Display preformance status
-    
-    
+
+  /* a) $0–2999 - Poor
+      c) $3000–4999 - Average
+      d) $5000–9999 - Good
+      e) $10000–14999 - Excellent
+      f) $15000 and above – Outstanding */
+
+    if (sales >= 0 && sales <= 2999)
+    {
+      Console.WriteLine("Performance is poor.");
+    }
+    else if (sales >= 3000 && sales <= 4999)
+    {
+      Console.WriteLine("Performance is average.");
+    }
+    else if (sales >= 5000 && sales <= 9999)
+    {
+      Console.WriteLine("Performance is good.");
+    }
+    else if (sales >= 10000 && sales <= 14999)
+    {
+      Console.WriteLine("Performance is excellent.");
+    }
+    else
+    {
+      Console.WriteLine("Performance is outstanding.");
+    }
 
 
 
